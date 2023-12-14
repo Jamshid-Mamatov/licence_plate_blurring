cfb52bfb7a77444988e7c0b435e857332f236c17
Jamshid-Mamatov <mamatovjamshid605@gmail.com>
2023-12-14 20:18:40 +0500

    car detection

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import numpy as np
 import cv2
+from ultralytics import YOLO
+from PIL import Image
+model_car_detection = YOLO('yolov8s.pt')
 
 cap = cv2.VideoCapture('20220906_122241_11.mp4')
 
@@ -9,8 +12,17 @@ while cap.isOpened():
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame', gray)
+
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    results=model_car_detection.predict(frame,classes=[2,5,7])
+    # Show the results
+    for r in results:
+        im_array = r.plot()  # plot a BGR numpy array of predictions
+        im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+        im.show()  # show image
+        # im.save('results.jpg')  # save image
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) == ord('q'):
         break
 cap.release()
